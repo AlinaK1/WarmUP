41ef3e64c28ff3b483e37d8b8861bd710822d694
Uladzimir Tsikhon <uladzimir_tsikhon@epam.com>
2018-12-20 22:32:31 +0300

    Add Unit tetst

---
+++ WarmUP/WarmUP/StringFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace StringHelper
+{
+    public static class StringOperations
+    {
+        public static int ConvertToInt(string data)
+        {
+            throw new NotImplementedException();
+        }
+        public static string RemoveSymbols(string str, char ch)
+        {
+
+            return "";
+        }
+
+    }
+}
+
+
+
---
+++ WarmUP/WarmUP/StringHelperTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+namespace StringHelper
+{
+    [TestFixture]
+    public class StrigFilterTest
+    {
+
+
+        [Test]
+        public void RemoveTest()
+        {
+            var actual = StringOperations.RemoveSymbols("dsdsd", '!');
+            Assert.AreEqual("dsdsd", actual);
+        }
+
+
+    }
+}
